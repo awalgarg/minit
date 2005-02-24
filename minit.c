12d0896439df4395682b729dfddb78677f213d90
leitner <leitner>
2005-02-24 09:10:08 +0000

    malloc -> alloca

--- minit.c
+++ minit.c
@@ -201,10 +201,10 @@ again:
       argv=split(s,'\n',&argc,2,1);
       if (argv[argc-1]) argv[argc-1]=0; else argv[argc]=0;
     } else {
-      argv=(char**)malloc(2*sizeof(char*));
+      argv=(char**)alloca(2*sizeof(char*));
       argv[1]=0;
     }
-    argv0=(char*)malloc(PATH_MAX+1);
+    argv0=(char*)alloca(PATH_MAX+1);
     if (!argv || !argv0) _exit(1);
     if (readlink("run",argv0,PATH_MAX)<0) {
       if (errno!=EINVAL) _exit(1);	/* not a symbolic link */
